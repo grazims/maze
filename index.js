b64fdf9577ad1984a32abdf038045805faba7b03
graziela silva <grazims26@gmail.com>
2020-07-22 13:25:30 -0700

    add drawing borders and shape + click and draging fuction

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  MouseConstraint,
+  Mouse
+} = Matter;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -13,7 +21,20 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-World.add(world, shape);
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+  })
+);
+
+//walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
