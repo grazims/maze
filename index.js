a9ea518d9587a248c8f4a041d36528bb4bb1b6d5
graziela silva <grazims26@gmail.com>
2020-07-22 14:19:51 -0700

    add grid

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ const walls = [
   Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }), //top
   Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }), //bottom
   Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }), //left
-  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }) //right
 ];
 World.add(world, walls);
+
+//maze grid
+
+const grid = Array(3)
+  .fill(null)
+  .map(() => Array(3).fill(false));
+
+const verticals = Array(3)
+  .fill(null)
+  .map(() => Array(2).fill(false));
+const horizontals = Array(2)
+  .fill(null)
+  .map(() => Array(3).fill(false));
