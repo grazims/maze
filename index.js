09ca4eaf99d988b9be83aed479b123da53cc86d0
graziela silva <grazims26@gmail.com>
2020-07-22 20:35:56 -0700

    add keyboard control and disable gravity

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
 const cells = 13;
 const width = 600;
@@ -7,6 +7,7 @@ const height = 600;
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -155,3 +156,20 @@ World.add(world, goal);
 //ball
 const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
 World.add(world, ball);
+
+document.addEventListener("keydown", event => {
+  const { x, y } = ball.velocity;
+
+  if (event.keyCode === 38) {
+    Body.setVelocity(ball, { x, y: y - 5 });
+  }
+  if (event.keyCode === 39) {
+    Body.setVelocity(ball, { x: x + 5, y });
+  }
+  if (event.keyCode === 40) {
+    Body.setVelocity(ball, { x, y: y + 5 });
+  }
+  if (event.keyCode === 37) {
+    Body.setVelocity(ball, { x: x - 5, y });
+  }
+});
